01d30bfbb3043451768fcf5540f4f1d81d2c160a
Yage0110 <151400661+Yage0110@users.noreply.github.com>
2024-04-27 03:09:54 -0400

    Update HW6.py

--- HW6.py
+++ HW6.py
@@ -1,6 +1,6 @@
 # Name: Jay Bacchus
 # Email: agyei.bacchus92@myhunter.cuny.edu
-#Program that prints numbers 0 to 14
+#Program that prints numbers in range 0 to 15
 for i in range(0,15):
     print(i)
     
